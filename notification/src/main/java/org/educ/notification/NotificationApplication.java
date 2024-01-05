7016105acee811ccd6171de495b9bdd1585b4fa4
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-05 02:45:53 +0200

    added Notification application main class

---
+++ notification/src/main/java/org/educ/notification/NotificationApplication.java
@@ -0,0 +1,40 @@
+package org.educ.notification;
+
+
+import org.educ.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.educ.notification",
+                "org.educ.amqp",
+        }
+)
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
+public class NotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//            ) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("Ali", 18),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
+}
